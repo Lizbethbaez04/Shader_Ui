d92f64296e0a58919e8fb4e211ffebb4cf744730
Lizbethbaez04 <42244092+Lizbethbaez04@users.noreply.github.com>
2019-05-08 10:35:12 -0500

    Initial commit

---
+++ Assets/Scripts/Albedo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Control de UI
+using UnityEngine.UI;
+
+public class Albedo : MonoBehaviour
+{
+    Renderer render;
+    [SerializeField]
+    Slider sld_ColorR;
+    [SerializeField]
+    Slider sld_ColorG;
+    [SerializeField]
+    Slider sld_ColorB;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        render = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        render.material.SetFloat("_ColorR", sld_ColorR.value);
+        render.material.SetFloat("_ColorG", sld_ColorG.value);
+        render.material.SetFloat("_ColorB", sld_ColorB.value);
+    }
+}
---
+++ Assets/Scripts/MyCharacter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Control de UI
+using UnityEngine.UI;
+
+public class MyCharacter : MonoBehaviour
+{
+    Renderer render;
+    [SerializeField]
+    float colorPower = 1f;
+    [SerializeField]
+    Slider sld_colorPower;
+
+    private void Start()
+    {
+        render = GetComponent<Renderer>();
+
+    }
+
+    private void Update()
+    {
+        render.material.SetFloat("_ColorPower", sld_colorPower.value);
+    }
+}
